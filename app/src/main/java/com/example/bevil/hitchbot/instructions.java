1584c3d23613f00138aa62bfeb43307b70f5b9cb
dbevila1 <dbevila1@kent.edu>
2017-12-03 13:33:55 -0500

    Added functionality for the NO button on the second screen.

--- app/src/main/java/com/example/bevil/hitchbot/instructions.java
+++ app/src/main/java/com/example/bevil/hitchbot/instructions.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 public class instructions extends AppCompatActivity {
 
     Button yesButt2;
+    Button noButt2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +21,16 @@ public class instructions extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 Intent myIntent = new Intent(instructions.this, MapsActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        noButt2 = (Button) findViewById(R.id.noButt2);
+        noButt2.setOnClickListener(new View.OnClickListener(){
+            /** Called when the user taps the YES button */
+            @Override
+            public void onClick(View v){
+                Intent myIntent = new Intent(instructions.this, MainActivity.class);
                 startActivity(myIntent);
             }
         });
